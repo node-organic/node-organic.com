045090683bf30736d8251f9b7367980af0da65c5
Boris Filipov <obiwonn@gmail.com>
2015-01-11 01:18:50 +0200

    upgrade and improved skeleton

--- scripts/static-files.js
+++ scripts/static-files.js
@@ -1,27 +1,38 @@
+var gulp = require("gulp")
+var path = require("path")
 var async = require("async")
-var exec = require("child_process").exec
 
-var processEntry = function(angel, entry, done) {
-  async.each(angel.dna.public[entry], function(folderpath, next){
-    exec("mkdir -p "+angel.dna.nginx.staticFolder+"/"+entry, function(err){
-      if(err) return next(err)
-      exec("cp -r "+folderpath+"/* "+angel.dna.nginx.staticFolder+"/"+entry, next)
+module.exports = function(angel){
+  angel.on("prepare static files", function(angel, next){
+    var publicData = require("../dna/public")
+    var nginxData = require("../dna/nginx")
+    var target = path.normalize(path.join(process.cwd(), nginxData.staticFolder))
+    var moved = []
+    var tasks = []
+    var move = function(sources, publicSuffix){
+      return function(done) {
+        var dest = target+"/"+publicSuffix
+        var sourcePaths = sources.map(function(path){
+          if(typeof path == "object")
+            return path.path+"/**/"+path.include
+          else
+            return path+"/**/*.*"
+        })
+        gulp.src(sourcePaths)
+          .pipe(gulp.dest(dest))
+          .on("end", function(){
+            moved.push({sources: sourcePaths, dest: dest})
+            done()
+          })
+          .on("error", done)
+      }
+    }
+    for(var mountUrl in publicData) {
+      tasks.push(move(publicData[mountUrl], mountUrl.replace("public", "")))
+    }
+    async.parallel(tasks, function(err){
+      next && next(err, moved)
+      console.info("Combination ",err?"FAILED":"SUCCESSFUL")
     })
-  }, done)
-}
-
-module.exports = function(angel, next){
-  var processing = 0
-  for(var entry in angel.dna.public) {
-    processing += 1
-    processEntry(angel, entry, function(err){
-      processing -= 1
-      if(err) {
-        console.error(err)
-        process.exit(1)
-      } else
-        console.info("success")  
-      if(processing == 0) next && next()
-    })
-  }
+  })
 }
